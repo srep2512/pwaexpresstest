3f577c716fef178979207dd0c26b2cde484a6d5e
Felix Brandt <felixbrandt2512@gmail.com>
2018-11-12 08:12:19 +0100

    init

---
+++ docs/precache-manifest.cec408e09d60c7a8ae1543e87373980e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwaexpresstest/robots.txt"
+  },
+  {
+    "revision": "ca43cb72ecbc07e56d3c",
+    "url": "/pwaexpresstest/js/chunk-vendors.d376f400.js"
+  },
+  {
+    "revision": "5fe096f5b9b488dbd2a5",
+    "url": "/pwaexpresstest/js/app.3f358186.js"
+  },
+  {
+    "revision": "73362790e234123431ae",
+    "url": "/pwaexpresstest/js/about.62b236ff.js"
+  },
+  {
+    "revision": "bb6dd32a59f93c09582167c0efcde3cb",
+    "url": "/pwaexpresstest/index.html"
+  },
+  {
+    "revision": "301f94aeeac34f6fa406b2c25a209323",
+    "url": "/pwaexpresstest/img/paypal.301f94ae.png"
+  },
+  {
+    "revision": "4437b7393deb7a1f0af7fbeaf0faa89c",
+    "url": "/pwaexpresstest/img/fot.png"
+  },
+  {
+    "revision": "ca43cb72ecbc07e56d3c",
+    "url": "/pwaexpresstest/css/chunk-vendors.78cb4066.css"
+  },
+  {
+    "revision": "5fe096f5b9b488dbd2a5",
+    "url": "/pwaexpresstest/css/app.b2eb4f61.css"
+  }
+];
